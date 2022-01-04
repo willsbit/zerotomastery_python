0427b016483169cdde1697cc5985c24a791df3f8
Willian <60491870+willsbit@users.noreply.github.com>
2022-01-04 15:41:45 -0300

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -7,12 +7,12 @@ answer = randint(int(sys.argv[1]), int(sys.argv[2]))
 while True:
     try:
         guess = int(input(f'guess a number {sys.argv[1]}~{sys.argv[2]}:  '))
-        if  0 < guess < 11:
+        if  sys.argv[1] < guess < sys.argv[2]:
             if guess == answer:
                 print('you are a genius!')
                 break
         else:
-            print('hey bozo, I said 1~10')
+            print(f'hey bozo, I said {sys.argv[1]}~{sys.argv[2]}')
     except ValueError:
         print('please enter a number')
         continue
