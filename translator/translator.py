24fe7fe20926a88fee3e1a61c6aa6aecccc18791
Willian <60491870+willsbit@users.noreply.github.com>
2022-01-05 10:30:59 -0300

    Update translator.py

--- translator/translator.py
+++ translator/translator.py
@@ -1,10 +1,11 @@
 from translate import Translator
 
-
-with open('./texto.txt') as text:
-    root_text = text.read()
-
-translator= Translator(to_lang="ja")
-translation = translator.translate(root_text)
+translator = Translator(to_lang="ja")
+try:
+    with open('./texto.txt') as text:
+        root_text = text.read()
+        translation = translator.translate(root_text)
+except FileNotFoundError as err:
+    print('Incorrect file path.')
 
 print(translation)
